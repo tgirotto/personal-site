a6a8987e52c8071b484735ac7caf2b39635f83a6
Tommaso Girotto <tommaso.girotto91@gmail.com>
2025-12-23 18:19:41 +0100

    changed settings in docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://tgirotto.github.io',
+  url: 'https://tommaso-girotto.co',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',        // The name of your repository
